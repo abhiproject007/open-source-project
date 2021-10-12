1dc04d27c2537644428cbb118b1c610f3017ac42
Abhishek Sahu <61976357+abhisahugm007@users.noreply.github.com>
2021-10-12 07:53:28 +0530

    Update amazon 3sum.java
    issue solve now working fine

--- amazon 3sum.java
+++ amazon 3sum.java
@@ -13,6 +13,12 @@
 public class 3Sum {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>>  result = new ArrayList<>();
+            Arrays.sort(nums);
+        
+        for(int i = 0; i < nums.length - 2; i++) {
+            if(i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
             
             int j = i + 1;
             int k = nums.length - 1;
